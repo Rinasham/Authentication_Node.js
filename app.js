f32893fc75f83aa4106a6450ba6b2e16d1f84d47
Rinasham <rinashamau@gmail.com>
2022-04-16 00:42:42 +1000

    [add] mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -9,6 +10,14 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
+
+
+
+//------- settings for DB -------------
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true})
+
+
 
 
 app.get('/', (req, res)=> {
